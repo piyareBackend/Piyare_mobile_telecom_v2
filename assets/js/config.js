ff9d595479c468dd76fa425b2c876e45d1c663ca
piyareBackend <sadab.notes.backup@gmail.com>
2026-08-21 18:58:55 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
 /* Public configuration. The Apps Script /exec URL is not a secret.
    Replace ONLY the value below before publishing the website. */
-window.PMT_PUBLIC_API_URL="PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE";
+window.PMT_PUBLIC_API_URL="https://script.google.com/macros/s/AKfycbwstzcqUNbrxIV-4cxgmi_s7Yp-JYGO4ozHwwascfG4c3dr5mAsxQatD_6fpfxruu4/exec";
